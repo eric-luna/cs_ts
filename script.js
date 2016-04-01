20cdac1ec03194309f70797b8df9d0cbede537a9
Eric <ericluna1992@gmail.com>
2016-04-01 05:56:23 -0500

    added some prev/next buttons anda  background image

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
   $('.title, .poster, .overview,.navigation').empty();
   var search=$('.search').val();
   var url ="http://api.themoviedb.org/3/search/movie?api_key=39124889ea92aada0703109651a543ab&query="+search;
-
   $.ajax(
     {
      url: url,
@@ -25,7 +24,7 @@ $(document).ready(function(){
         var title=result.results[count]['title'];
         var poster = result.results[count]['poster_path'];
         var overview=result.results[count]['overview'];
-       
+
         $('.title').append("<h1>"+title+"</h1>");
         if(poster === null ){
           $('.poster').append("<img src='img/noPoster.jpg'>");
@@ -33,16 +32,19 @@ $(document).ready(function(){
         if(poster !== null){
            $('.poster').append("<img src='http://image.tmdb.org/t/p/w500/"+poster+"'>");
         }
+        if(overview ===""){
+           $('.overview').append("<p>No Description Available</p>");
+        }else{
+           $('.overview').append("<p>"+overview+"</p>");
+        }
        
-        $('.overview').append("<p>"+overview+"</p>");
         if(count > 0){
-          $('.navigation').append("<button class='arrow left'></button>");
-          $('.navigation').append("<button class='arrow right'></button>");
+          $('.navigation').append("<button class='left action-button animate blue'>Prev</button>");
+          $('.navigation').append("<button class='right action-button animate blue'>Next</button>");
         }
         if(count===0){
-           $('.navigation').append("<button class='arrow right'></button>");
+           $('.navigation').append("<button class='right action-button animate blue'>Next</button>");
         }
-       
         $('.right').on('click',function(){
           if(count < result.results.length){
             count=count+1;
@@ -53,29 +55,21 @@ $(document).ready(function(){
             $('button').click();
           }
         });
-       
        $('.left').on('click',function(){
           if(count > 0){
             count=count-1;
             $('button').click();
           }
         });
-       
        $(document).keydown(function(e) {
         if (e.keyCode == 37)
         $('.left').click();
        });
-       
        $(document).keydown(function(e) {
         if (e.keyCode == 39)
         $('.right').click();
-      });
-       
-       
-         
+      }); 
        console.log(result.results.length);
      }})
   })
-  
-  
 });
