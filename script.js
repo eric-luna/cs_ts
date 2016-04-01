3752a00789dc2b220f0b03f9ab71a88ea060baa3
Eric <ericluna1992@gmail.com>
2016-04-01 03:22:32 -0500

    this is the inital commit for the movie app Cinema Seeker. I laid teh bluepint out in codepen and have transferred the code into my repo folder

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
-  $('.search').keypress(function(e) {
+ 
+  var count=0;
+  
+   $('.search').keypress(function(e) {
     if (e.keyCode == 13)
       $('.button').click();
+     count=0;
   });
-  
+
   $('.button').on('click',function(){
-  $('.poster').empty();
+  $('.title, .poster, .overview,.navigation').empty();
   var search=$('.search').val();
   var url ="http://api.themoviedb.org/3/search/movie?api_key=39124889ea92aada0703109651a543ab&query="+search;
-  console.log(url);
+
   $.ajax(
     {
      url: url,
@@ -17,9 +21,59 @@ $(document).ready(function(){
      contentType: 'application/json',
      dataType: 'jsonp',
      success: function(result){
-        var path = result.results[0];
-        $('.poster').append("<img src='http://image.tmdb.org/t/p/w500/"+result.results[0]['poster_path']+"'>");
-       console.log(result.results[0]['poster_path']);
+        
+        var title=result.results[count]['title'];
+        var poster = result.results[count]['poster_path'];
+        var overview=result.results[count]['overview'];
+       
+        $('.title').append("<h1>"+title+"</h1>");
+        if(poster === null ){
+          $('.poster').append("<img src='img/noPoster.jpg'>");
+        }
+        if(poster !== null){
+           $('.poster').append("<img src='http://image.tmdb.org/t/p/w500/"+poster+"'>");
+        }
+       
+        $('.overview').append("<p>"+overview+"</p>");
+        if(count > 0){
+          $('.navigation').append("<button class='arrow left'></button>");
+          $('.navigation').append("<button class='arrow right'></button>");
+        }
+        if(count===0){
+           $('.navigation').append("<button class='arrow right'></button>");
+        }
+       
+        $('.right').on('click',function(){
+          if(count < result.results.length){
+            count=count+1;
+            $('button').click();
+          }
+          if(!(count < result.results.length)){
+            count=0;
+            $('button').click();
+          }
+        });
+       
+       $('.left').on('click',function(){
+          if(count > 0){
+            count=count-1;
+            $('button').click();
+          }
+        });
+       
+       $(document).keydown(function(e) {
+        if (e.keyCode == 37)
+        $('.left').click();
+       });
+       
+       $(document).keydown(function(e) {
+        if (e.keyCode == 39)
+        $('.right').click();
+      });
+       
+       
+         
+       console.log(result.results.length);
      }})
   })
   
